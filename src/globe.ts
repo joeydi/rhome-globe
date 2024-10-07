9f3873ee3426d936c481f1e6741ce941989f4d83
Joe di Stefano <joeydi@okaypl.us>
2024-10-07 16:28:42 -0400

    move marker formatting to updateMarkers function, call on init

--- src/globe.ts
+++ src/globe.ts
@@ -4,10 +4,32 @@ import data from "./data.json";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
-var mapRange = function (inMin: number, inMax: number, outMin: number, outMax: number) {
+const mapRange = (inMin: number, inMax: number, outMin: number, outMax: number) => {
   return function scale(value: number) {
     return outMin + (outMax - outMin) * ((value - inMin) / (inMax - inMin));
   };
+};
+
+const scaleZ = mapRange(0, 90, 1000, 0);
+const scaleBlur = mapRange(3000, 10000, 0, 10);
+const scaleScale = mapRange(0, 10000, 1, 0.5);
+
+const updateMarkers = (markers, map) => {
+  const center = map.getCenter();
+
+  for (const marker of markers) {
+    const distance = center.distanceTo(marker.getLngLat()) / 1000;
+    const blur = scaleBlur(distance);
+    const scale = scaleScale(distance);
+
+    const markerElement = marker.getElement();
+    const cardElement: HTMLDivElement | null = markerElement.querySelector(".card");
+
+    if (markerElement && cardElement) {
+      markerElement.style.filter = `blur(${blur}px)`;
+      cardElement.style.scale = `${scale}`;
+    }
+  }
 };
 
 export function initGlobe(element: HTMLDivElement | null) {
@@ -32,50 +54,33 @@ export function initGlobe(element: HTMLDivElement | null) {
 
   const markers: Marker[] = [];
 
-  const zScale = mapRange(0, 90, 1000, 0);
-
   for (const feature of data) {
     const el = document.createElement("div");
     el.className = "marker";
     el.innerHTML = `
-        <div class="card">
-            <img src="${import.meta.env.VITE_ASSET_ROOT}${feature.image}" alt="" />
-            <div class="details">
-                <span>${feature.price}</span>
-                <span>${feature.type}</span>
-            </div>
+      <div class="card">
+        <img src="${import.meta.env.VITE_ASSET_ROOT}${feature.image}" alt="" />
+        <div class="details">
+          <span>${feature.price}</span>
+          <span>${feature.type}</span>
         </div>
-        <div class="flag">
-            <img src="${import.meta.env.VITE_ASSET_ROOT}/flags/${feature.country}.svg" alt="" />
-        </div>`;
+      </div>
+      <div class="flag">
+        <img src="${import.meta.env.VITE_ASSET_ROOT}/flags/${feature.country}.svg" alt="" />
+      </div>`;
 
     const marker = new mapboxgl.Marker({ element: el, anchor: "bottom" }).setLngLat([feature.lng, feature.lat]).addTo(map);
 
-    const zIndex = Math.floor(zScale(feature.lat)).toString();
+    const zIndex = Math.floor(scaleZ(feature.lat)).toString();
     const element = marker.getElement();
     element.style.zIndex = zIndex;
 
     markers.push(marker);
   }
 
-  const scaleBlur = mapRange(3000, 10000, 0, 10);
-  const scaleScale = mapRange(0, 10000, 1, 0.5);
+  updateMarkers(markers, map);
 
   map.on("move", () => {
-    const center = map.getCenter();
-
-    for (const marker of markers) {
-      const distance = center.distanceTo(marker.getLngLat()) / 1000;
-      const blur = scaleBlur(distance);
-      const scale = scaleScale(distance);
-
-      const markerElement = marker.getElement();
-      const cardElement: HTMLDivElement | null = markerElement.querySelector(".card");
-
-      if (markerElement && cardElement) {
-        markerElement.style.filter = `blur(${blur}px)`;
-        cardElement.style.scale = `${scale}`;
-      }
-    }
+    updateMarkers(markers, map);
   });
 }
